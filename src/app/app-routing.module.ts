b416678e960bcffa17fc22492048909889de6ed6
Nicolas Adam <nic.adam@bluewin.ch>
2023-01-13 19:31:58 +0100

    main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 
+
 const routes: Routes = [
   {path: '', component: MainPageComponent},
   {path: 'upload', loadChildren: () => import('./pages/upload-pages/upload-pages.module').then(m=>m.UploadPagesModule)}
